50c3f6e911c4b781b741d7a4550df1b03f878fdf
Tedros Alem <tedsaasfaha@gmail.com>
2024-10-14 00:02:40 +0300

    [Stack first commit] String reverse problem implementation

---
+++ data structures and algorithms/src/stack/reverse_string/StringReverserDemo.java
@@ -0,0 +1,13 @@
+package stack.reverse_string;
+
+public class StringReverserDemo {
+
+    public static void main(String[] args) {
+        String str = null;
+
+        StringReverser reverser = new StringReverser();
+        System.out.println(reverser.reverse(str));
+
+    }
+
+}
